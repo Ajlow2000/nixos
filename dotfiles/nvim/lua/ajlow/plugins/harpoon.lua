421274f8691985bc7f1d0cd68ee5a3c654a2816d
ajlow2000 <ajlow2000@gmail.com>
2023-11-07 17:49:29 +0000

    fix(nvim): Updated keybind descriptions for harpoon bindings

--- dotfiles/nvim/lua/ajlow/plugins/harpoon.lua
+++ dotfiles/nvim/lua/ajlow/plugins/harpoon.lua
@@ -28,10 +28,10 @@ return {
 
         vim.keymap.set("n", "<leader>h", ":lua require('harpoon.ui').toggle_quick_menu()<cr>", { desc = "[Harpoon] - Toggle Harpoon Menu", silent = true })
         vim.keymap.set("n", "<leader>hh", ":lua require('harpoon.mark').add_file()<cr>", { desc = "[Harpoon] - Add file to harpoon" })
-        vim.keymap.set("n", "<leader>f", ":lua require('harpoon.ui').nav_file(1)<cr>", { desc = "Navigate to file 1" } )
-        vim.keymap.set("n", "<leader>d", ":lua require('harpoon.ui').nav_file(2)<cr>", { desc = "Navigate to file 2" } )
-        vim.keymap.set("n", "<leader>s", ":lua require('harpoon.ui').nav_file(3)<cr>", { desc = "Navigate to file 3" } )
-        vim.keymap.set("n", "<leader>a", ":lua require('harpoon.ui').nav_file(4)<cr>", { desc = "Navigate to file 4" } )
+        vim.keymap.set("n", "<leader>f", ":lua require('harpoon.ui').nav_file(1)<cr>", { desc = "[Harpoon] - Navigate to file 1" } )
+        vim.keymap.set("n", "<leader>d", ":lua require('harpoon.ui').nav_file(2)<cr>", { desc = "[Harpoon] - Navigate to file 2" } )
+        vim.keymap.set("n", "<leader>s", ":lua require('harpoon.ui').nav_file(3)<cr>", { desc = "[Harpoon] - Navigate to file 3" } )
+        vim.keymap.set("n", "<leader>a", ":lua require('harpoon.ui').nav_file(4)<cr>", { desc = "[Harpoon] - Navigate to file 4" } )
         -- vim.keymap.set("n", "<C-l>", ":lua require('harpoon.ui').nav_next()<cr>", { desc="[Harpoon] - Navigate to next Harpoon file"})
         -- vim.keymap.set("n", "<C-h>", ":lua require('harpoon.ui').nav_prev()<cr>", { desc = "[Harpoon] - Navigate to previous Harpoon file"}) require('harpoon.ui').toggle_quick_menu()
     end
