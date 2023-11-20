459dfddf09ca5b32fa44173ad6b0629912f1129b
ajlow2000 <ajlow2000@gmail.com>
2023-11-20 12:04:08 -0700

    feat(nvim): Add typst support

--- dotfiles/nvim/lua/ajlow/plugins/nvim-lspconfig.lua
+++ dotfiles/nvim/lua/ajlow/plugins/nvim-lspconfig.lua
@@ -97,5 +97,11 @@ return {
                 }
             }
         })
+        lspconfig.typst_lsp.setup{
+            settings = {
+                exportPdf = "onType" -- Choose onType, onSave or never.
+                -- serverPath = "" -- Normally, there is no need to uncomment it.
+            }
+        }
     end
 }
