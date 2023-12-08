b3bfede8c9bb9d6984ae0b0742e13573e9c7a027
ajlow2000 <ajlow2000@gmail.com>
2023-12-08 18:08:38 +0000

    feat(nvim): Fix order of imports for using util funcs

--- dotfiles/nvim/lua/ajlow/init.lua
+++ dotfiles/nvim/lua/ajlow/init.lua
@@ -1,12 +1,5 @@
 vim.g.maplocalleader = " "
 
--- Conditionally add a playground file for local testing
---  This playground file is not tracked/deployed with home manager
---  to allow for local testing without a full HM rebuild.
---  NOTE: additions to playground will not be tracked with VCS
-if is_module_available("ajlow.utils.playground") then
-    require("ajlow.utils.playground")
-end
 require("ajlow.utils.debug")
 require("ajlow.utils.custom")
 require("ajlow.std.options")
@@ -14,4 +7,11 @@ require("ajlow.std.keymaps")
 require("ajlow.std.autocommands")
 require("ajlow.lazy")
 
+-- Conditionally add a playground file for local testing
+--  This playground file is not tracked/deployed with home manager
+--  to allow for local testing without a full HM rebuild.
+--  NOTE: additions to playground will not be tracked with VCS
+if is_module_available("ajlow.utils.playground") then
+    require("ajlow.utils.playground")
+end
 
