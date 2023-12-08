80fdfaff8130f2119599f4805c71824045d2e424
ajlow2000 <ajlow2000@gmail.com>
2023-12-08 16:47:13 +0000

    feat(nvim): Remove lazy status from lualine

--- dotfiles/nvim/lua/ajlow/plugins/lualine.lua
+++ dotfiles/nvim/lua/ajlow/plugins/lualine.lua
@@ -8,7 +8,6 @@ return {
     },
     config = function()
         local lualine = require("lualine")
-        local lazy_status = require("lazy.status") -- to configure lazy pending updates count
 
         lualine.setup({
             sections = {
@@ -17,11 +16,6 @@ return {
                     "filename",
                 },
                 lualine_x = {
-                    {
-                        lazy_status.updates,
-                        cond = lazy_status.has_updates,
-                        color = { fg = "#ff9e64" },
-                    },
                     { "encoding" },
                     { "fileformat" },
                     { "filetype" },
