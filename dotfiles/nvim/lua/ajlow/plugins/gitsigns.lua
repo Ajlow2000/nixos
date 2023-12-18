dacbfff609aa9a2bca472a5fd8a255b0a3b9e94e
ajlow2000 <ajlow2000@gmail.com>
2023-12-18 23:03:50 +0000

    feat(nvim): Update git line blame formatting

--- dotfiles/nvim/lua/ajlow/plugins/gitsigns.lua
+++ dotfiles/nvim/lua/ajlow/plugins/gitsigns.lua
@@ -13,7 +13,7 @@ return {
             untracked    = { text = '┆' },
         },
         signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
-        numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
+        numhl      = true, -- Toggle with `:Gitsigns toggle_numhl`
         linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
         word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
         watch_gitdir = {
@@ -24,10 +24,10 @@ return {
         current_line_blame_opts = {
             virt_text = true,
             virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-            delay = 1000,
+            delay = 0,
             ignore_whitespace = false,
         },
-        current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+        current_line_blame_formatter = '<author> | <author_time:%Y-%m-%d> | <summary>',
         sign_priority = 6,
         update_debounce = 100,
         status_formatter = nil, -- Use default
@@ -43,5 +43,5 @@ return {
         yadm = {
             enable = false
         },
-    } 
+    }
 }
