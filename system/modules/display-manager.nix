75c5b4b305e24b957eca58e8b6a9642c326488b6
ajlow2000 <ajlow2000@gmail.com>
2024-06-23 22:13:10 -0600

    feat(dm): Migrate to sddm with remote theme

--- system/modules/display-manager.nix
+++ system/modules/display-manager.nix
@@ -1,6 +1,30 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 let 
     cfg = config.display-manager;
+
+    image = pkgs.fetchurl {
+        url = "https://raw.githubusercontent.com/Goxore/dotfiles/50db864d56d49768f1d4d0a8c1bd7a5c74dd629e/home/Wallpapers/gruvbox-mountain-village.png";
+        sha256 = "sha256-HrcYriKliK2QN02/2vFK/osFjTT1NamhGKik3tozGU0=";
+    };
+
+    sddmTheme = pkgs.stdenv.mkDerivation {
+        name = "sddm-theme";
+
+        src = pkgs.fetchFromGitHub {
+          owner = "MarianArlt";
+          repo = "sddm-sugar-dark";
+          rev = "ceb2c455663429be03ba62d9f898c571650ef7fe";
+          sha256 = "0153z1kylbhc9d12nxy9vpn0spxgrhgy36wy37pk6ysq7akaqlvy";
+        };
+
+        installPhase = ''
+          mkdir -p $out
+          cp -R ./* $out/
+          cd $out/
+          rm Background.jpg
+          cp -r ${image} $out/Background.jpg
+        '';
+    };
 in {
     options = {
         display-manager.enable = lib.mkOption {
@@ -10,7 +34,12 @@ in {
     };
 
     config = lib.mkIf cfg.enable {
-        services.xserver.displayManager.lightdm.enable = true;
-        services.xserver.displayManager.lightdm.greeters.slick.enable = true;
+        services.displayManager.sddm.enable = true;
+        services.displayManager.sddm.theme = "${sddmTheme}";
+        # services.displayManager.sddm.wayland.enable = true;
+
+        environment.systemPackages = with pkgs; [
+            libsForQt5.qt5.qtgraphicaleffects
+        ];
     };
 }
