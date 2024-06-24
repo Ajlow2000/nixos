4791150484d45f65b183d601e18b68a4f0b0dbbb
ajlow2000 <ajlow2000@gmail.com>
2024-06-23 19:47:27 -0600

    refactor!: Add enable option to pde and env
    BREAKING CHANGE:

--- userspace/archetype/personal_user.nix
+++ userspace/archetype/personal_user.nix
@@ -9,6 +9,9 @@
         ../modules/de.nix
     ];
 
+    pde.enable = true;
+    env.enable = true;
+
     # This value determines the Home Manager release that your configuration is
     # compatible with. This helps avoid breakage when a new Home Manager release
     # introduces backwards incompatible changes.
