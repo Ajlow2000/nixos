13ee48933793066adb7122dd14027fc338f460fd
ajlow2000 <ajlow2000@gmail.com>
2024-07-02 14:32:47 +0000

    feat(nvim): Move undodir to .local/share

--- userspace/dotfiles/nvim/lua/ajlow/std/options.lua
+++ userspace/dotfiles/nvim/lua/ajlow/std/options.lua
@@ -4,7 +4,7 @@ vim.opt.relativenumber = true                   -- set relative numbered lines
 vim.opt.cursorline = true                       -- highlight the current line
 vim.opt.numberwidth = 4                         -- set number column width to 2 {default 4}
 vim.opt.signcolumn = "yes"                      -- always show the sign column, otherwise it would shift the text each time
-vim.opt.cmdheight = 1                           -- Control height of 
+vim.opt.cmdheight = 1                           -- Control height of cmd bar
 vim.opt.mouse = "a"                             -- allow the mouse to be used in neovim
 vim.opt.showtabline = 0                         -- hide tabline
 vim.opt.splitbelow = true                       -- force all horizontal splits to go below current window
@@ -28,18 +28,18 @@ vim.opt.sidescrolloff = 8                       -- minimal number of screen colu
 vim.opt.fileencoding = "utf-8"                  -- the encoding written to a file
 vim.opt.conceallevel = 0                        -- so that `` is visible in markdown files
 vim.opt.pumheight = 10                          -- pop up menu max height
-vim.opt.completeopt = { "menuone", "noselect" }  -- mostly just for cmp
+vim.opt.completeopt = { "menuone", "noselect" } -- mostly just for cmp
 vim.opt.termguicolors = true                    -- set term gui colors (most terminals support this)
 vim.opt.timeoutlen = 400
 vim.opt.updatetime = 750
 
 vim.opt.writebackup = false
 vim.opt.undofile = true                         -- enable persistent undo
-vim.opt.undodir = os.getenv("HOME") .. "/.nvim/undodir"
+vim.opt.undodir = os.getenv("HOME") .. "/.local/share/nvim/undodir"
 vim.opt.swapfile = false                        -- creates a swapfile
 vim.opt.backup = false                          -- creates a backup file
 vim.opt.whichwrap = "bs<>[]hl"                  -- which "horizontal" keys are allowed to travel to prev/next line
-vim.opt.shortmess:append "c"                           -- don't give |ins-completion-menu| messages
-vim.opt.iskeyword:append "-"                           -- hyphenated words recognized by searches
+vim.opt.shortmess:append "c"                    -- don't give |ins-completion-menu| messages
+vim.opt.iskeyword:append "-"                    -- hyphenated words recognized by searches
 
 vim.opt.runtimepath:remove("/usr/share/vim/vimfiles")  -- separate vim plugins from neovim in case vim still in use
