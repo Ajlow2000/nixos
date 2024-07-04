52ac6c08ce2a91feb9dba3d2f1e1f9ac403680c6
ajlow2000 <ajlow2000@gmail.com>
2024-07-04 13:04:54 -0600

    wip: pushing changes for testing on microvac

--- userspace/modules/my_hypr.nix
+++ userspace/modules/my_hypr.nix
@@ -14,15 +14,15 @@ in {
         wayland.windowManager.hyprland.settings = {
             "$mod" = "SUPER";
             bind = [
-                ",              Print,  exec,           grimblast copy area"
-                "$mod,          RETURN, exec,           kitty tmux-session-manager"
-                "$mod SHIFT,    RETURN, exec,           firefox"
-                "$mod,          DELETE, killactive, "
-                "$mod SHIFT,    DELETE, exit, "
-                "$mod,          M,      togglefloating, "
-                "$mod,          SPACE,  exec,           wofi --show drun"
-                "$mod,          P,      pseudo, "
-                "$mod,          J,      togglesplit, "
+                ",              Print,      exec,           grimblast copy area"
+                "$mod,          RETURN,     exec,           kitty tmux-session-manager"
+                "$mod SHIFT,    RETURN,     exec,           firefox"
+                "$mod,          BACKSPACE,  exit, "
+                "$mod SHIFT,    BACKSPACE,  killactive, "
+                "$mod,          M,          togglefloating, "
+                "$mod,          SPACE,      exec,           wofi --show drun"
+                "$mod,          P,          pseudo, "
+                "$mod,          J,          togglesplit, "
             ];
         };
     };
