915e51e2bd38ff0e17ede8c1e15f3be0f030e1c2
ajlow2000 <ajlow2000@gmail.com>
2024-07-04 14:54:38 -0600

    wip: pushing changes for testing on microvac

--- userspace/modules/my_hypr.nix
+++ userspace/modules/my_hypr.nix
@@ -29,8 +29,8 @@ in {
                     "$mod, k, movefocus, u"
                     "$mod, j, movefocus, d"
 
-                    "$mod, u, workspace, m-1"
-                    "$mod, i, workspace, m+1"
+                    "$mod,          Tab, workspace, r+1"
+                    "$mod SHIFT,    Tab, workspace, r-1"
 
                     "$mod, 1, workspace, 1"
                     "$mod, 2, workspace, 2"
