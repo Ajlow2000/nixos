92d2e947798ea2c659c3a60f8f33beeb9dc87398
ajlow2000 <ajlow2000@gmail.com>
2024-07-04 13:49:59 -0600

    wip: pushing changes for testing on microvac

--- userspace/modules/my_hypr.nix
+++ userspace/modules/my_hypr.nix
@@ -10,62 +10,88 @@ in {
     };
 
     config = lib.mkIf cfg.enable {
-        wayland.windowManager.hyprland.enable = true;
-        wayland.windowManager.hyprland.settings = {
-            "$mod" = "SUPER";
-            bind = [
-                "$mod,              RETURN,     exec,           kitty tmux-session-manager"
-                "$mod SHIFT,        RETURN,     exec,           firefox"
-                "$mod,              q,          exit, "
-                "$mod SHIFT,        c,          killactive, "
-                "$mod,              f,          fullscreen, 1"
-                "$mod SHIFT,        f,          fullscreen, 0"
-                ",                  f11,        fullscreen, 0"
-                "$mod CONTROL SHIFT,f,          fullscreen, 2"
+        wayland.windowManager.hyprland = {
+            enable = true;
+            settings = {
+                "$mod" = "SUPER";
+                bind = [
+                    "$mod,              RETURN,     exec,           kitty tmux-session-manager"
+                    "$mod SHIFT,        RETURN,     exec,           firefox"
+                    "$mod,              q,          exit, "
+                    "$mod SHIFT,        c,          killactive, "
+                    "$mod,              f,          fullscreen, 1"
+                    "$mod SHIFT,        f,          fullscreen, 0"
+                    ",                  f11,        fullscreen, 0"
+                    "$mod CONTROL SHIFT,f,          fullscreen, 2"
 
-                "$mod, h, movefocus, l"
-                "$mod, l, movefocus, r"
-                "$mod, k, movefocus, u"
-                "$mod, j, movefocus, d"
+                    "$mod, h, movefocus, l"
+                    "$mod, l, movefocus, r"
+                    "$mod, k, movefocus, u"
+                    "$mod, j, movefocus, d"
 
-                "$mod, 1, workspace, 1"
-                "$mod, 2, workspace, 2"
-                "$mod, 3, workspace, 3"
-                "$mod, 4, workspace, 4"
-                "$mod, 5, workspace, 5"
-                "$mod, 6, workspace, 6"
-                "$mod, 7, workspace, 7"
-                "$mod, 8, workspace, 8"
-                "$mod, 9, workspace, 9"
-                "$mod, 0, workspace, 10"
+                    "$mod, 1, workspace, 1"
+                    "$mod, 2, workspace, 2"
+                    "$mod, 3, workspace, 3"
+                    "$mod, 4, workspace, 4"
+                    "$mod, 5, workspace, 5"
+                    "$mod, 6, workspace, 6"
+                    "$mod, 7, workspace, 7"
+                    "$mod, 8, workspace, 8"
+                    "$mod, 9, workspace, 9"
+                    "$mod, 0, workspace, 10"
 
-                "$mod SHIFT, 1, movetoworkspace, 1"
-                "$mod SHIFT, 2, movetoworkspace, 2"
-                "$mod SHIFT, 3, movetoworkspace, 3"
-                "$mod SHIFT, 4, movetoworkspace, 4"
-                "$mod SHIFT, 5, movetoworkspace, 5"
-                "$mod SHIFT, 6, movetoworkspace, 6"
-                "$mod SHIFT, 7, movetoworkspace, 7"
-                "$mod SHIFT, 8, movetoworkspace, 8"
-                "$mod SHIFT, 9, movetoworkspace, 9"
-                "$mod SHIFT, 0, movetoworkspace, 10"
+                    "$mod SHIFT, 1, movetoworkspace, 1"
+                    "$mod SHIFT, 2, movetoworkspace, 2"
+                    "$mod SHIFT, 3, movetoworkspace, 3"
+                    "$mod SHIFT, 4, movetoworkspace, 4"
+                    "$mod SHIFT, 5, movetoworkspace, 5"
+                    "$mod SHIFT, 6, movetoworkspace, 6"
+                    "$mod SHIFT, 7, movetoworkspace, 7"
+                    "$mod SHIFT, 8, movetoworkspace, 8"
+                    "$mod SHIFT, 9, movetoworkspace, 9"
+                    "$mod SHIFT, 0, movetoworkspace, 10"
 
-                "$mod, mouse:272, movewindow"   # mod + LMB
-                "$mod, mouse:273, resizewindowpixel"      # mod + RMB
+                    "$mod, mouse:272, movewindow"   # mod + LMB
+                    "$mod, mouse:273, resizewindowpixel"      # mod + RMB
 
-                # Scroll through workspaces with mod + scroll
-                "$mod, mouse_down, workspace, m+1"
-                "$mod, mouse_up, workspace, m-1"
+                    # Scroll through workspaces with mod + scroll
+                    "$mod, mouse_down, workspace, m+1"
+                    "$mod, mouse_up, workspace, m-1"
 
-                "$mod,              m,          togglefloating, "
-                "$mod,              P,          pseudo, "
-                "$mod,              J,          togglesplit, "
+                    "$mod,              m,          togglefloating, "
+                    "$mod,              P,          pseudo, "
+                    "$mod,              J,          togglesplit, "
 
-                "$mod,              SPACE,      exec,           wofi --show drun"
-                ",                  Print,      exec,           grimblast copy area"
-            ];
-            gestures = {
-                workspace_swipe = "on";
+                    "$mod,              SPACE,      exec,           wofi --show drun"
+                    ",                  Print,      exec,           grimblast copy area"
+                ];
+
+                general = {
+                    layout = "dwindle"; # dwindle | master
+
+                    gaps_in = 5;
+                    gaps_out = 5;
+                    border_size = 2;
+                    col.active_border = "rgba(d3c6aaff)";
+                    # col.inactive_border = "rgba(1e2326)";
+                };
+
+                input = {
+                    kb_layout = "us";
+                    kb_options = "ctrl:nocaps";
+
+                    follow_mouse = 1;
+
+                    touchpad = {
+                        natural_scroll = 1;
+                    };
+
+                    sensitivity = 0; # -1.0 - 1.0, 0 means no modification.
+                };
+
+                gestures = {
+                    workspace_swipe = "on";
+                };
             };
         };
     };
