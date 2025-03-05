3c43a6506901c0c852f13badf833de45e059d27c
Alec Lowry <accounts@aleclowry.com>
2025-03-05 11:09:31 -0700

    feat(nvim): typst lsp -> tinymist

--- userspace/dotfiles/nvim/lua/ajlow/plugins/nvim-lspconfig.lua
+++ userspace/dotfiles/nvim/lua/ajlow/plugins/nvim-lspconfig.lua
@@ -98,7 +98,7 @@ return {
                 }
             }
         })
-        lspconfig.typst_lsp.setup{
+        lspconfig.tinymist.setup{
             settings = {
                 exportPdf = "onType" -- Choose onType, onSave or never.
                 -- serverPath = "" -- Normally, there is no need to uncomment it.
