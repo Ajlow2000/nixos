111ae12b53edad70d22ae1be48afe8f07a168d44
Alec Lowry <accounts@aleclowry.com>
2026-01-17 13:21:18 -0700

    feat: stock lackluster

--- system/archetype/personal.nix
+++ system/archetype/personal.nix
@@ -29,6 +29,7 @@ in {
         i18n.inputMethod = {
             enable = true;
             type = "fcitx5";
+            fcitx5.waylandFrontend = true; # Crucial for native Wayland apps
             fcitx5.addons = with pkgs; [
                 qt6Packages.fcitx5-chinese-addons # Contains the Pinyin engine
                 fcitx5-gtk            # For GTK apps (Chrome, etc.)
@@ -38,12 +39,27 @@ in {
         };
 
         fonts.packages = with pkgs; [
+            nerd-fonts.hack
             noto-fonts
             noto-fonts-cjk-sans
             noto-fonts-cjk-serif
             wqy_microhei  # Popular compact Chinese font
             wqy_zenhei
         ];
+
+        fonts.fontconfig.defaultFonts = {
+            monospace = [ "Hack Nerd Font" "Noto Sans Mono CJK SC" ];
+        };
+
+        # # Force Wayland apps to recognize the Input Method
+        # environment.sessionVariables = {
+        #     XMODIFIERS = "@im=fcitx";
+        #     QT_IM_MODULE = "fcitx";
+        #     GTK_IM_MODULE = "fcitx";
+        #     # Some Rust/COSMIC apps need this specifically:
+        #     CLUTTER_IM_MODULE = "fcitx";
+        #     GLFW_IM_MODULE = "ibus"; # Fcitx5 emulates the ibus protocol for GLFW/Rust apps
+        # };
 
         networking.firewall.checkReversePath = false;
         environment.systemPackages = with pkgs; [
