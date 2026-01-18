c09ada9f85d10e7245efc1f0115dc094570a8cf2
Alec Lowry <accounts@aleclowry.com>
2026-01-18 13:44:23 -0700

    feat: language keyboard stuff

--- system/archetype/personal.nix
+++ system/archetype/personal.nix
@@ -33,6 +33,7 @@ in {
             fcitx5.addons = with pkgs; [
                 qt6Packages.fcitx5-chinese-addons # Contains the Pinyin engine
                 fcitx5-gtk            # For GTK apps (Chrome, etc.)
+                fcitx5-m17n
                 fcitx5-rime           # Alternative: RIME (highly customizable)
                 # fcitx5-chewing      # For Traditional Chinese (optional)
             ];
@@ -47,19 +48,13 @@ in {
             wqy_zenhei
         ];
 
+        environment.sessionVariables = {
+            XMODIFIERS = "@im=fcitx";
+        };
+
         fonts.fontconfig.defaultFonts = {
             monospace = [ "Hack Nerd Font" "Noto Sans Mono CJK SC" ];
         };
-
-        # # Force Wayland apps to recognize the Input Method
-        # environment.sessionVariables = {
-        #     XMODIFIERS = "@im=fcitx";
-        #     QT_IM_MODULE = "fcitx";
-        #     GTK_IM_MODULE = "fcitx";
-        #     # Some Rust/COSMIC apps need this specifically:
-        #     CLUTTER_IM_MODULE = "fcitx";
-        #     GLFW_IM_MODULE = "ibus"; # Fcitx5 emulates the ibus protocol for GLFW/Rust apps
-        # };
 
         networking.firewall.checkReversePath = false;
         environment.systemPackages = with pkgs; [
