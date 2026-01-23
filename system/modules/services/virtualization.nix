6b2f45fd9001c98ff46066422d00b4a8b28b3930
Alec Lowry <accounts@aleclowry.com>
2026-01-22 22:45:02 -0700

    feat: pin qemu

--- system/modules/services/virtualization.nix
+++ system/modules/services/virtualization.nix
@@ -1,6 +1,15 @@
 { config, lib, pkgs, ... }:
 let
     cfg = config.modules.services.virtualization;
+
+    # Pin QEMU to a specific nixpkgs commit to avoid VM boot issues after updates
+    # Current pinned commit: 3497aa5c9457a9d88d71fa93a4a8368816fbeeba (current flake.lock)
+    # To update QEMU: change the rev below, rebuild, then update your VM configs
+    pinnedNixpkgs = import (builtins.fetchTarball {
+        url = "https://github.com/NixOS/nixpkgs/archive/3497aa5c9457a9d88d71fa93a4a8368816fbeeba.tar.gz";
+    }) { inherit (pkgs) system; };
+
+    pinnedQemu = pinnedNixpkgs.qemu_kvm;
 in {
     options.modules.services.virtualization = {
         enable = lib.mkEnableOption "libvirt/QEMU virtualization with virt-manager";
@@ -9,6 +18,12 @@ in {
             type = lib.types.listOf lib.types.str;
             default = [];
             description = "Users to add to virtualization groups (libvirtd, kvm, qemu-libvirtd)";
+        };
+
+        pinQemu = lib.mkOption {
+            type = lib.types.bool;
+            default = true;
+            description = "Pin QEMU to a specific version to prevent VM boot issues after updates";
         };
     };
 
@@ -35,7 +50,10 @@ in {
 
         virtualisation.libvirtd = {
             enable = true;
-            qemu.swtpm.enable = true;
+            qemu = {
+                package = lib.mkIf cfg.pinQemu pinnedQemu;
+                swtpm.enable = true;
+            };
         };
     };
 }
