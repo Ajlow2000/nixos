52585199296d8575bac3022c44d5d035bdc2cfcb
Alec Lowry <accounts@aleclowry.com>
2026-03-09 09:49:41 -0600

    fix: i think s1 is `rfkill block bluetooth` -ing on boot lol

--- system/profiles/laptop.nix
+++ system/profiles/laptop.nix
@@ -15,5 +15,19 @@ in {
         profiles.system.desktop.enable = true;
 
         modules.hardware.power.enable = true;
+
+        hardware.bluetooth.enable = true;
+        hardware.bluetooth.powerOnBoot = true;
+        services.blueman.enable = true;
+
+        systemd.services.bluetooth-rfkill-unblock = {
+            description = "Unblock Bluetooth rfkill soft block";
+            before = [ "bluetooth.service" ];
+            wantedBy = [ "bluetooth.service" ];
+            serviceConfig = {
+                Type = "oneshot";
+                ExecStart = "${pkgs.util-linux}/bin/rfkill unblock bluetooth";
+            };
+        };
     };
 }
