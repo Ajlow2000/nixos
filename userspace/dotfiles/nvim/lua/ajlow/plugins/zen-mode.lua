1ffe6a98d0e29d6b8ebf9b685dc9b2e0518175f7
Alec Lowry <accounts@aleclowry.com>
2026-04-21 15:42:27 -0600

    feat: zenmode for markdown

---
+++ userspace/dotfiles/nvim/lua/ajlow/plugins/zen-mode.lua
@@ -0,0 +1,51 @@
+return {
+    "folke/zen-mode.nvim",
+    config = function()
+        require("zen-mode").setup({
+            window = {
+                backdrop = 0.95,
+                width = 80,
+                height = .95,
+                options = {
+                    signcolumn = "no",
+                    number = false,
+                    relativenumber = false,
+                    cursorline = false,
+                    foldcolumn = "0",
+                    wrap = true,
+                    linebreak = true,
+                },
+            },
+            plugins = {
+                options = {
+                    enabled = true,
+                    laststatus = 0,
+                },
+                gitsigns = { enabled = false },
+                tmux = { enabled = false },
+                todo = { enabled = false },
+            },
+            on_open = function()
+                vim.keymap.set("n", "j", "gj", { buffer = true })
+                vim.keymap.set("n", "k", "gk", { buffer = true })
+            end,
+            on_close = function()
+                vim.keymap.del("n", "j", { buffer = true })
+                vim.keymap.del("n", "k", { buffer = true })
+            end,
+        })
+
+        vim.keymap.set("n", "<leader>z", function()
+            require("zen-mode").toggle()
+        end, { desc = "[ZenMode] - Toggle Zen Mode" })
+
+        vim.api.nvim_create_autocmd("BufWinEnter", {
+            pattern = "*.md",
+            callback = function()
+                vim.schedule(function()
+                    require("zen-mode").open()
+                end)
+            end,
+        })
+    end,
+}
