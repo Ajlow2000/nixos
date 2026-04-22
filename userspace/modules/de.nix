3bac0f8ae273e80a4a051e447dd14f89fcc422dd
Alec Lowry <accounts@aleclowry.com>
2026-04-21 21:50:14 -0600

    fix: i hate gnome-keyring bor

--- userspace/modules/de.nix
+++ userspace/modules/de.nix
@@ -1,4 +1,4 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 let
   cfg = config.de;
 in
@@ -16,5 +16,24 @@ in
 
   config = lib.mkIf cfg.enable {
     my_hypr.enable = true;
+
+    # Prevent gnome-keyring from starting via XDG autostart.
+    # User entries in ~/.config/autostart/ override /etc/xdg/autostart/.
+    xdg.configFile."autostart/gnome-keyring-pkcs11.desktop".text = ''
+      [Desktop Entry]
+      Hidden=true
+    '';
+    xdg.configFile."autostart/gnome-keyring-secrets.desktop".text = ''
+      [Desktop Entry]
+      Hidden=true
+    '';
+
+    # Shadow the D-Bus service file so gnome-keyring can't be activated on-demand.
+    # User-level files in ~/.local/share/dbus-1/services/ take precedence over system ones.
+    xdg.dataFile."dbus-1/services/org.freedesktop.secrets.service".text = ''
+      [D-BUS Service]
+      Name=org.freedesktop.secrets
+      Exec=${pkgs.coreutils}/bin/false
+    '';
   };
 }
