efec179d000e2d268dffaa62dc9c077243c2b7eb
Alec Lowry <accounts@aleclowry.com>
2026-06-23 00:34:15 -0600

    feat: generate sops ssh keys and root passwds for the rest of my hosts

--- keys.nix
+++ keys.nix
@@ -5,7 +5,16 @@
       hal9000       = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPnRHwhbaOEEuUnfHjek3WKbSCXFnEZBJ2zgs2l3DXeP root@hal9000";
       microvac      = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIiP8fRhoOUXLwWmtPfX361r7cv8VCryKtKoZ21PSVhT root@microvac_2026-06-22";
       mindgame      = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIMRnK9Uo43JWuNSH/dOc6/ALyEtms/AXMqGWgsLIdPK root@mindgame_2026-06-22";
+      multivac      = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMGguSQMuMvnVka3kOF9kRcneoIIcEcOUFzNH1W1sCbV root@multivac_2026-06-22";
+
+      glados        = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK+YdLApxmf3xBlxIB/EBALKyo7yeJX4M8ZcKRmAyk1L root@glados_2026-06-22";
       do-prod-01    = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJgVRvyuY4hNXqo2a1q+T+R4gtmptYGQ6xWtYliB0bke root@do-prod-01_2026-06-22";
+
+      eddie         = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL6zrTX2LeTIuZMLAOLPQQ3d9PClRTR70ZMvJQFvq5a7 root@eddie_2026-06-22";
+      marvin        = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEuvq/P4AEwGCihjBr74dOiOhrL4ZfFwzPCPxma+6NSl root@marvin_2026-06-22";
+
+      installer     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINvhqWvyb6keA47PF2p5Q82/KlRBZIHW4MujQVdjDhri root@installer_2026-06-22";
+      do-base-image = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGXAt9qFZUfQpe5Wlf+0lsNGZFujH2c7UkwxqQWrplHP root@do-base-image_2026-06-22";
   };
 
 
